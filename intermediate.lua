79e6a8b9dca65395ac4b86e78e4b611f9c39d1c8
trm0024 <trm0024@uah.edu>
2018-11-04 15:43:49 -0600

    Added button to go back to title on both win and loss, as well as added win sound

--- intermediate.lua
+++ intermediate.lua
@@ -8,14 +8,18 @@
 -----------------------------------------------------------------------------------------
 
 local composer = require("composer")
+local widget = require("widget")
 
 local scene = composer.newScene()
 
 -- Keep track of lives and stage here
 local lives = 4
 local stage = 1
+local titleButton;
 
 local gotoGameTimer--the timer for automatically going to the game
+
+local winSnd = audio.loadSound("win.wav")
 
 -- Listener function for switching to game scene, happens after a couple seconds
 local function switchSceneToGame( event )
@@ -39,6 +43,16 @@ local function gotoGame ()
 	end
 	gotoGameTimer = timer.performWithDelay(2500, switchSceneToGame)
 end
+--listens for titlebutton upon game win or loss
+local function titleButtonListener(event)
+	if("ended" == event.phase) then
+		composer.removeScene("intermediate")
+		composer.gotoScene("title")
+
+	end
+end
+
+
 
 function scene:create( event )
 	local sceneGroup = self.view
@@ -50,6 +64,28 @@ function scene:create( event )
 	backRect:setFillColor(1, 1, 1, 0)
 	sceneGroup:insert(backRect)
 	backRect:addEventListener("tap", switchSceneToGame)
+
+	titleButton = widget.newButton(
+				{
+				onEvent = titleButtonListener,
+				emboss = false,
+				shape = "circle",
+				radius = 35,
+				cornerRadius = 2,
+				fillColor = {default={0,0,1,1}, over={1, 1, 1, 1}},
+				strokeColor = {default ={1,1,1,1}, over={1,1,1,1}},
+				strokeWidth = 4
+			
+				}
+			)
+	titleButton.x = display.contentCenterX
+	titleButton.y = display.contentCenterY + 100
+	titleButton.isVisible = false;
+
+	titleText = display.newText(sceneGroup, "Return to Title Screen", display.contentCenterX, display.contentCenterY + 50, native.systemFont, 20);
+	titleText.isVisible = false;
+
+	sceneGroup:insert(titleButton)
 	
 	-- Shows the number of lives remaining at the top of the screen
 	livesDisplay = display.newText(sceneGroup, lives.." Lives Remain", display.contentCenterX, 20, native.systemFont, 24)
@@ -58,6 +94,7 @@ function scene:create( event )
 	
 	-- Shows the current stage of the player
 	stageDisplay = display.newText(sceneGroup, "Stage "..stage, display.contentCenterX, display.contentCenterY, native.systemFont, 28)
+	
 end
 
 function scene:show( event )
@@ -82,15 +119,22 @@ function scene:show( event )
 			--continue to next round
 			livesDisplay.text = lives.." Lives Remain"
 			stageDisplay.text = "Stage "..stage
+			titleButton.isVisible = false;
+			titleText.isVisible = false;
 			gotoGame()-- Switch to game view after a few seconds
 		elseif(lives <= 0) then
 			--lose game
 			livesDisplay.text = ""
 			stageDisplay.text = "Game Over!"
+			titleButton.isVisible = true;
+			titleText.isVisible = true;
 		elseif(stage > 10) then
 			--win game
 			livesDisplay.text = ""
 			stageDisplay.text = "You win!"
+			titleButton.isVisible = true;
+			titleText.isVisible = true;
+			audio.play(winSnd)
 		end
 	end
 end
